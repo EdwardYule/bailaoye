24bfdb54aec384ac071388a08667f115fda3020d
Edward Chan <31300219+EdwardYule@users.noreply.github.com>
2024-11-18 10:39:39 +0800

    ✨ feat: 初步逻辑

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+import Logo from './test.jpg'
+
+const touchStartY = ref(0)
+const touchEndY = ref(0)
+const handStyle = ref({
+  transform: 'translateY(0px)',
+})
+
+function handleTouchStart(event: TouchEvent) {
+  touchStartY.value = event.touches[0].clientY
+}
+
+function handleTouchMove(event: TouchEvent) {
+  touchEndY.value = event.touches[0].clientY
+  const deltaY = touchEndY.value - touchStartY.value
+  handStyle.value = {
+    transform: `translate(-50%, ${deltaY}px)`,
+  }
+}
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main @touchstart="handleTouchStart" @touchmove="handleTouchMove">
+    <div class="hand" :style="handStyle">
+      <img :src="Logo" alt="Hand" />
+    </div>
   </main>
 </template>
+
+<style scoped lang="less">
+.hand {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
+
